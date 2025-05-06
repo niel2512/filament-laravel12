59e687bdfce7b5dae34adad0b700d7fdac855cfe
niel2512 <nielyusuf17@gmail.com>
2025-05-06 20:48:31 +0700

    Edit hero icon

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -17,7 +17,7 @@ class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-banknotes';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
 
     public static function form(Form $form): Form
     {
